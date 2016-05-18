367ebb13c9a21d59633d307fafc12337b46888eb
Arturo Borrero <arturo.borrero.glez@gmail.com>
2016-05-18 19:05:12 +0200

    conntrackd: add support for NTA_(S|D)NAT_IPV6
    So we can properly sync NATed IPv6 connections.
    
    Thanks to Florian Westphal for originally ponting me to this lack of
    support in conntrackd, which saved me a lot of time.
    
    Signed-off-by: Arturo Borrero Gonzalez <arturo.borrero.glez@gmail.com>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- include/network.h
+++ include/network.h
@@ -229,6 +229,8 @@ enum nta_attr {
 	NTA_TCP_WSCALE_REPL,	/* uint8_t */
 	NTA_HELPER_NAME,	/* string (variable length) */
 	NTA_LABELS,		/* array of uint32_t (variable length) */
+	NTA_SNAT_IPV6,		/* struct nfct_attr_grp_ipv6 */
+	NTA_DNAT_IPV6,		/* struct nfct_attr_grp_ipv6 */
 	NTA_MAX
 };
 
